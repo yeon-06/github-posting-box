4ee894c722a51bb4087d8b41352c9492f296a4ff
yeon-06 <rnjstldus2@gmail.com>
2022-12-06 16:12:23 +0900

    chore: 프로젝트 초기화

---
+++ src/main/java/com/github/postingbox/PostingboxApplication.java
@@ -0,0 +1,13 @@
+package com.github.postingbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostingboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostingboxApplication.class, args);
+	}
+
+}
