1d81e43bafe1b4ab5f5addcfcfa9ce13318bf7ad
yeon-06 <rnjstldus2@gmail.com>
2023-01-02 16:48:50 +0900

    refactor: 포스팅 형식 파일에서 불러오기

--- src/main/java/com/github/postingbox/service/PostingService.java
+++ src/main/java/com/github/postingbox/service/PostingService.java
@@ -6,8 +6,12 @@ import com.github.postingbox.domain.Boards;
 import com.github.postingbox.domain.GitHubInfo;
 import com.github.postingbox.support.GitHubClient;
 import com.github.postingbox.support.HtmlSupporter;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
 import java.util.stream.Collectors;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -16,6 +20,17 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class PostingService {
+
+    private static final int COLUMN_SIZE = 3;
+    private static final String LINE_SEPARATOR = System.lineSeparator();
+    private static final String BOARD_INFO_FORMAT = "<td>" + LINE_SEPARATOR
+            + "    <a href=\"%s\">" + LINE_SEPARATOR
+            + "        <img width=\"100%%\" src=\"%s\"/><br/>" + LINE_SEPARATOR
+            + "        <div>%s</div>" + LINE_SEPARATOR
+            + "    </a>" + LINE_SEPARATOR
+            + "    <div>%s</div>" + LINE_SEPARATOR
+            + "    <div>%s</div>" + LINE_SEPARATOR
+            + "</td>";
 
     private final BlogInfo blogInfo;
     private final HtmlSupporter htmlSupporter;
@@ -62,20 +77,56 @@ public class PostingService {
     }
 
     private String generateContents(final Boards boards) {
-        return "### \uD83D\uDE80 About Me\n"
-                + "\n"
-                + "- 안녕하세요! 신입 백엔드 개발자 권시연 입니다.\n"
-                + "- 블로그, 깃허브 등 꾸준한 기록 활동을 통해 **꾸준함을 증명**할 수 있습니다.\n"
-                + "- **지식을 공유하며 소통**하는 활동을 좋아하여 [한이음](https://www.hanium.or.kr/portal/index.do), [글또](https://www.notion.so/ac5b18a482fb4df497d4e8257ad4d516), [MeetCoder](https://github.com/Meet-Coder-Study/posting-review) 등 20여 개 이상의 스터디 또는 멘토 멘티에 참여 하였습니다.\n"
-                + "- **모르는 것을 빠르게 질문**하고, 자신의 의견을 드러내는 것에 불편함이 없습니다.\n"
-                + "- 유지보수하기 좋은 코드가 누구든지 이해할 수 있는, 좋은 코드라고 생각합니다.\n"
-                + "\n"
-                + "<br/>\n"
-                + "\n"
-                + "### \uD83D\uDCDA Blog\n"
-                + "\n"
-                + boards.generateContents(blogInfo.getUrl())
-                + "\n";
+        try {
+            FileReader fileReader = new FileReader("./src/main/resources/templates/default.md");
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            String fileContents = bufferedReader.lines()
+                    .collect(Collectors.joining(LINE_SEPARATOR));
+            return fileContents + LINE_SEPARATOR + generateContents(boards, blogInfo.getUrl());
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private String generateContents(final Boards boards, final String blogUrl) {
+        List<Board> value = boards.getValue();
+        int size = value.size();
+
+        StringBuilder stringBuilder = new StringBuilder().append("<table><tbody>");
+
+        for (int i = 0; i < size; i++) {
+            if (i % COLUMN_SIZE == 0) {
+                stringBuilder.append("<tr>")
+                        .append(LINE_SEPARATOR);
+            }
+
+            stringBuilder.append(generateTdTag(blogUrl, value.get(i)))
+                    .append(LINE_SEPARATOR);
+
+            if (i % COLUMN_SIZE == COLUMN_SIZE - 1 || i == size - 1) {
+                stringBuilder.append("</tr>")
+                        .append(LINE_SEPARATOR);
+            }
+        }
+        stringBuilder.append("</tbody></table>");
+
+        return stringBuilder.toString();
+    }
+
+    private String generateTdTag(final String blogUrl, final Board board) {
+        return String.format(BOARD_INFO_FORMAT,
+                blogUrl + board.getLink(),
+                "https:" + board.getImageUrl(),
+                board.getTitle(),
+                board.getSummary().substring(0, 50) + "...",
+                toDate(board)
+        );
+    }
+
+    private String toDate(final Board board) {
+        LocalDate date = board.getDate();
+        return String.format("%d.%02d.%02d", date.getYear() % 100, date.getMonthValue(), date.getDayOfMonth());
     }
 
     private String generateCommitMessage() {
