798f04061f280c90db1b70fd3a3547e0475052a1
yeon-06 <yeonlog06@gmail.com>
2023-07-30 14:22:48 +0900

    feat: print my job

--- src/main/java/com/github/postingbox/HelloApplication.java
+++ src/main/java/com/github/postingbox/HelloApplication.java
@@ -5,5 +5,6 @@ public class HelloApplication {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("My name is YeonLog");
+        System.out.println("I'm a developer");
     }
 }
