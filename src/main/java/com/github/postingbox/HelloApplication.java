bfa6004ee6103bae347ad9900801b9706877788b
yeon-06 <yeonlog06@gmail.com>
2023-07-30 16:48:58 +0900

    feat: print Hello YeonLog!

--- src/main/java/com/github/postingbox/HelloApplication.java
+++ src/main/java/com/github/postingbox/HelloApplication.java
@@ -4,5 +4,6 @@ public class HelloApplication {
 
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println("Hello, YeonLog!");
     }
 }
